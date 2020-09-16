63b1ea47bd657ee8e823ee803300e57538f38cdb
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-09-16 10:17:14 +0700

    finish

--- LevelB04/Source.cpp
+++ LevelB04/Source.cpp
@@ -2,7 +2,7 @@
 void drawtriangle(int x)
 {
 	for (int i = 1; i <= x; i++)
-	{  for (int j = 1; j <= i; j++) { printf("* "); }
+	{  for (int j = 1; j <= i; j++) { printf("%d ",j); }
 		printf("\n");
 	}
 }
