7acc1061a6c176fbabc461f6406c474c81b55d85
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-09-16 10:04:32 +0700

    Add project files.

---
+++ LevelB04/Source.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void drawtriangle(int x)
+{
+	for (int i = 1; i <= x; i++)
+	{  for (int j = 1; j <= i; j++) { printf("* "); }
+		printf("\n");
+	}
+}
+int main()
+{
+	int num;
+	scanf_s("%d", &num);
+	drawtriangle(num);
+	return 0;
+}
